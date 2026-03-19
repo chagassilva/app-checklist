83364894bb58dcd7688b20922ce4b1bc0c92da5f
chagassilva <chagassilvati@gmail.com>
2026-03-18 23:44:52 -0300

    Initial

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "tld-app-v3";
+const urlsToCache = ["./", "./index.html", "./manifest.json"];
+
+// Instala o motor no celular
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
+
+// Faz o app carregar rápido buscando do cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
